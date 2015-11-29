bb3ee063fc4dc89679bb793bc4aeef412d78fb0f
Jean <jeancfbiezus@gmail.com>
2015-11-29 21:53:40 -0200

    Meu primeiro commit

---
+++ src/main/java/com/projetomaven/App.java
@@ -0,0 +1,13 @@
+package com.projetomaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
